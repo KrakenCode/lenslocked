5786aa6efa123759c93a14d851efc76bc3ec1d0d
krakencode <daltonsumrall@gmail.com>
2021-12-01 16:57:28 -0600

    Add contact page

--- main.go
+++ main.go
@@ -5,12 +5,19 @@ import (
 	"net/http"
 )
 
-func homeHandlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact page</h1><p>Email me at <a href=\"mailto:example@gmail.com\">example@gmail.com</a></p>")
+}
+
 func main() {
-	http.HandleFunc("/", homeHandlerFunc)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 
 	fmt.Println("Starting the server on :3000...")
 	err := http.ListenAndServe(":3000", nil)
