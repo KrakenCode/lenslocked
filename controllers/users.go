cb833abebe1bfd6c2edbb67021a343b3053a738f
krakencode <daltonsumrall@gmail.com>
2022-01-05 12:31:35 -0600

    Add query parameter handling for signup page

--- controllers/users.go
+++ controllers/users.go
@@ -12,7 +12,13 @@ type Users struct {
 }
 
 func (u Users) SignUp(w http.ResponseWriter, r *http.Request) {
-	u.Templates.SignUp.Execute(w, nil)
+	var data struct {
+		Email string
+	}
+
+	data.Email = r.FormValue("email")
+
+	u.Templates.SignUp.Execute(w, data)
 }
 
 func (u Users) CreateUser(w http.ResponseWriter, r *http.Request) {
